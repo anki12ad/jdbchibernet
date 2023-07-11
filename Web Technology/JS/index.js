899b601d77a101b1ab82efdc644474097e768078
Ankitha Vinay <ankithams2001@gmail.com>
2023-07-11 12:09:18 +0530

    webfiles

---
+++ Web Technology/JS/index.js
@@ -0,0 +1,72 @@
+// var a=10;
+// console.log(a);
+
+// alert("welcome to webpages")
+
+// confirm("welcome to webpages")
+// if(confirm("are existing user"))
+// {
+//     alert("welcome to my pages")
+// }
+// else{
+//     alert("Signup to the page")
+// }
+
+// var num1=prompt("Enter the number")
+// var num2=prompt("Enter the number")
+// var ans=num1+num2
+// alert("ur ans is"+ans)
+
+// var a;
+// a=10;
+// a=20;
+// console.log(a);
+
+// let b;
+// b=22;
+// b=30;
+// console.log(b);
+
+// const a=10;
+// console.log(a);
+
+// let b;
+// b="hello"
+// console.log(b);
+
+// let b=30;
+// console.log(b);
+// var a=10;
+//         console.log(a);
+
+// var num=12
+// var num1="a"
+// console.log(num+num1);
+
+// var num=12
+// var num1=15
+// console.log(num+num1);
+
+// var num=123.456
+// console.log(num.toFixed(1));
+
+// var num=243.87
+// console.log(num.toPrecision(4));
+
+// var num=23
+// console.log(typeof num.toString());
+
+// var num=1234
+// let count=0
+// while(num!=0)
+// {
+//     let rem=num%10
+//     count++
+//     num=parseInt(num/10)
+// }
+// console.log(count);
+
+var num=10
+var num1=3
+var ans=num/num1
+console.log(ans);
